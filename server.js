d203d0b030af059a8d385bc8223f2717b52a1a61
AELCharis <AELCharis>
2019-02-25 08:26:53 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors'); //sindeei to front me to backend secure
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors())
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            password: 'cookies',
+            email: 'john@gmail.com',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            password: 'bananas',
+            email: 'sally@gmail.com',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+         }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found = false;
+   database.users.forEach(user => {
+       if (user.id === id){ //ean aftos o user vrethi
+           found=true;  //kane to variabole true
+          return res.json(user);//kane response user json k
+       }
+   })
+    if (!found){
+        res.status(400).json('not found');
+    }
+})
+
+app.post('/signin', (req, res) => {
+
+
+    if(req.body.email === database.users[0].email &&
+       req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){ //ean aftos o user vrethi
+            found=true;  //kane to variabole true
+            user.entries++
+            return res.json(user.entries);//kane response user json k
+
+        }
+    })
+    if (!found){
+        res.status(400).json('not found');
+    }
+})
+
+app.post('/register', (req, res) => {
+        const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+        // Store hash in your password DB.
+    });
+        database.users.push ({
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        })
+    res.json(database.users[database.users.length-1]); //perni ton telefteo user pou dimiourgithike
+})
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('Applications its running on port 3000');
+})
